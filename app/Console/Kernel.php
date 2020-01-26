de962a0b3c593269a6eb7582614ad78341dbf327
Chidi Nkwocha <profchydon@gmail.com>
2020-01-26 01:49:04 +0100

    Changed Icons

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,7 +25,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->command('process:transaction')
-                 ->dailyAt('01:30');
+                 ->daily();
     }
 
     /**
