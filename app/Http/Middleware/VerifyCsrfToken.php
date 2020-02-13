322a17be935e9d2f657a053f67e17364ab729947
Chidi Nkwocha <profchydon@gmail.com>
2020-02-13 10:30:41 +0100

    Finished

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -19,6 +19,8 @@ class VerifyCsrfToken extends Middleware
      * @var array
      */
     protected $except = [
-        '/mail/register/success'
+        '/mail/register/success',
+        'mail/password/reset'
+
     ];
 }
